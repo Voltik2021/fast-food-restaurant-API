f420f8b42148abaff634b556559729ae2dfdb4a6
unknown <Voltik2019@yandex.ru>
2021-06-22 00:16:50 +0400

    shortened the lists

--- server/server.js
+++ server/server.js
@@ -6,42 +6,32 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 let arrCategory = [
-    {id:1, name: 'Бургеры', products: [1, 2, 3, 4, 5, 6]},
-    {id:2, name: 'Твистеры', products: [7, 8, 9, 10, 11, 12]},
-    {id:3, name: 'Курица', products: [13, 14, 15, 16, 17, 18]},
-    {id:4, name: 'Баскеты', products: [19, 20, 21, 22,]},
-    {id:5, name: 'Снеки', products: [23, 24, 25, 26, 27, 28]},  
+    { id: 1, name: 'Бургеры', products: [1, 2, 3, 4, 5, 6] },
+    { id: 2, name: 'Твистеры', products: [7, 8, 9, 10, 11, 12] },
+    { id: 3, name: 'Курица', products: [13, 14, 15, 16, 17, 18] },
+    { id: 4, name: 'Хиты', products: [4, 5, 7, 14, 16] },
+    { id: 5, name: 'Новое', products: [2, 3, 8, 13, 15] }
 ]
 
 let products = [
-    {id:1, name: 'Бургер', price:100, delivery:true, img:"#"},
-    {id:2, name: 'Бургер', price:100, delivery:true, img:"#"},
-    {id:3, name: 'Бургер', price:100, delivery:true, img:"#"},
-    {id:4, name: 'Бургер', price:100, delivery:true, img:"#"},
-    {id:5, name: 'Бургер', price:100, delivery:true, img:"#"},
-    {id:6, name: 'Бургер', price:100, delivery:true, img:"#"},
-    {id:7, name: 'Твистер', price:100, delivery:true, img:"#"},
-    {id:8, name: 'Твистер', price:100, delivery:true, img:"#"},
-    {id:9, name: 'Твистер', price:100, delivery:true, img:"#"},
-    {id:10, name: 'Твистер', price:100, delivery:true, img:"#"},
-    {id:11, name: 'Твистер', price:100, delivery:true, img:"#"},
-    {id:12, name: 'Твистер', price:100, delivery:true, img:"#"},
-    {id:13, name: 'Курица', price:100, delivery:true, img:"#"},
-    {id:14, name: 'Курица', price:100, delivery:true, img:"#"},
-    {id:15, name: 'Курица', price:100, delivery:true, img:"#"},
-    {id:16, name: 'Курица', price:100, delivery:true, img:"#"},
-    {id:17, name: 'Курица', price:100, delivery:true, img:"#"},
-    {id:18, name: 'Курица', price:100, delivery:true, img:"#"},
-    {id:19, name: 'Баскет', price:100, delivery:true, img:"#"},
-    {id:20, name: 'Баскет', price:100, delivery:true, img:"#"},
-    {id:21, name: 'Баскет', price:100, delivery:true, img:"#"},
-    {id:22, name: 'Баскет', price:100, delivery:true, img:"#"},
-    {id:23, name: 'Снеки', price:100, delivery:true, img:"#"},
-    {id:24, name: 'Снеки', price:100, delivery:true, img:"#"},
-    {id:25, name: 'Снеки', price:100, delivery:true, img:"#"},
-    {id:26, name: 'Снеки', price:100, delivery:true, img:"#"},
-    {id:27, name: 'Снеки', price:100, delivery:true, img:"#"},
-    {id:28, name: 'Снеки', price:100, delivery:true, img:"#"}
+    { id: 1, name: 'Бургер', price: 100, delivery: true, img: './src/img/burger.png' },
+    { id: 2, name: 'Бургер', price: 100, delivery: true, img: './src/img/newBurger.png' },
+    { id: 3, name: 'Бургер', price: 100, delivery: true, img: './src/img/newBurger.png' },
+    { id: 4, name: 'Бургер', price: 100, delivery: true, img: './src/img/hitBurger.png' },
+    { id: 5, name: 'Бургер', price: 100, delivery: true, img: './src/img/hitBurger.png' },
+    { id: 6, name: 'Бургер', price: 100, delivery: true, img: './src/img/burger.png' },
+    { id: 7, name: 'Твистер', price: 100, delivery: true, img: './src/img/hitTwister.png' },
+    { id: 8, name: 'Твистер', price: 100, delivery: true, img: './src/img/newTwister.png' },
+    { id: 9, name: 'Твистер', price: 100, delivery: true, img: './src/img/twister.png' },
+    { id: 10, name: 'Твистер', price: 100, delivery: true, img: './src/img/twister.png' },
+    { id: 11, name: 'Твистер', price: 100, delivery: true, img: './src/img/twister.png' },
+    { id: 12, name: 'Твистер', price: 100, delivery: true, img: './src/img/twister.png' },
+    { id: 13, name: 'Курица', price: 100, delivery: true, img: './src/img/newСhicken.png' },
+    { id: 14, name: 'Курица', price: 100, delivery: true, img: './src/img/hitСhicken.png' },
+    { id: 15, name: 'Курица', price: 100, delivery: true, img: './src/img/newСhicken.png' },
+    { id: 16, name: 'Курица', price: 100, delivery: true, img: './src/img/hitСhicken.png' },
+    { id: 17, name: 'Курица', price: 100, delivery: true, img: './src/img/chicken.png' },
+    { id: 18, name: 'Курица', price: 100, delivery: true, img: './src/img/chicken.png' }   
 ]
 
 let orders = []
